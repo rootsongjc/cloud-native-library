5fe96d1493b3a871205e0838ed2140f9633119d4
rootsongjc <rootsongjc@users.noreply.github.com>
2023-10-17 13:44:39 +0000

    deploy: 362d48984ad042f9ae628b7bcfbb0a0f424cc5ae

--- tsb-en/assets/quickstart/send-requests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while true; do
-    result=$(curl -m 5 -k -s -o /dev/null -I -w "%{http_code}" "https://bookinfo.tetrate.com/productpage" --connect-to bookinfo.tetrate.com:443:$GATEWAY_IP)
-    echo date: $(date),  status code: "$result"
-    sleep 1
-done
